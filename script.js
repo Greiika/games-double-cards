f05765e51da32ac061ba13d98361e15d05abe6b5
Max <kotiik97@mail.ru>
2024-12-25 13:18:47 +0300

    system balance, bonus x2, help poisk card

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ import imgBackVierUndZwanZig from './arrImgs/arrImgBackVierUndZwanZig.js';
 
 let parentCard = document.querySelector('.game-wrapper');
 let clickItem = document.querySelectorAll('.card');
+let walletBalance = document.querySelector('.wallet-honeycomb');
+
+// background
+
+let bodyBg = document.querySelector('body');
+bodyBg.style.backgroundImage = 'url(img/bg/bgCardUp-1.jpg)';
+bodyBg.style.backgroundRepeat = 'no-repeat';
+bodyBg.style.backgroundSize = 'cover';
+
+
+let honeyComb = 10; 
 
 function doubleCard(arr) {
-    
+
     let ArrImgBack = [];
-    let count = 1;
 
     ArrImgBack.splice(0, ArrImgBack.length);
 
@@ -23,8 +33,7 @@ function doubleCard(arr) {
     let RandomImg = ArrImgBack.sort(() => (Math.random() > .5) ? 2 : -1);
 
     for (let i = 0; i < ArrImgBack.length; i++) {
-        count = i;
-        console.log(count)
+        // count = i;
         let card = document.createElement('div');
         card.className = 'card';
 
@@ -47,102 +56,165 @@ function doubleCard(arr) {
         parentCard.appendChild(card);
     };
 
-    let matchedCard = 0;
-    let cardOne, cardTwo, cardOneImg, cardTwoImg;
-    let disableDeck = false;
-
     clickItem = document.querySelectorAll('.card');
 
     clickItem.forEach(elem => {
         elem.addEventListener('click', clickCard);
     });
-
-    function clickCard(e) {
-        let clickedCard = e.target;
-        if (clickedCard != cardOne && !disableDeck) {
-            clickedCard.classList.add('visible');
-            if (!cardOne) {
-                return (cardOne = clickedCard);
-            }
-        
-            cardTwo = clickedCard;
-            disableDeck = true;
-
-            cardOneImg = cardOne.querySelector('img').src,
-            cardTwoImg = cardTwo.querySelector('img').src;
-            matchCards(cardOneImg, cardTwoImg);
-        }
-    }
-
-    function matchCards(img1, img2) {
-        if (img1 == img2) {
-            matchedCard++;
-            console.log(count)
-            if (matchedCard == 4 && matchedCard == count ) {
-                setTimeout(() => {
-                    shuffleCard();
-                }, 1000)
-            }
-            cardOne.removeEventListener('click', clickCard);
-            cardTwo.removeEventListener('click', clickCard);
-
-            cardOne = cardTwo = '';
-            return (disableDeck = false);
-        }
-
-        
-        setTimeout(() =>  {
-            cardOne.classList.add('shake');
-            cardTwo.classList.add('shake');
-        }, 400)
-
-
-        setTimeout(() => {
-            cardOne.classList.remove('visible', 'shake');
-            cardTwo.classList.remove('visible', 'shake');
-            cardOne = cardTwo = '';
-            disableDeck = false;
-        }, 1200);
-    };
-
-    function shuffleCard() {
-        matchedCard = 0;
-        cardOne = cardTwo = '';
-        disableDeck = false;
-
-        clickItem.forEach(elem => {
-            elem.classList.remove('visible');
-
-            elem.addEventListener('click', clickCard);
-        });
-    };
-
-}
+};
 
 doubleCard(imgBackAcht);
 
 
+let matchedCard = 0;
+let cardOne, cardTwo, cardOneImg, cardTwoImg;
+let disableDeck = false;
+
+function clickCard(e) {
+    let clickedCard = e.target;
+
+    if (clickedCard != cardOne && !disableDeck) {
+        clickedCard.classList.add('visible');
+        if (!cardOne) {
+            return (cardOne = clickedCard);
+        }
+    
+        cardTwo = clickedCard;
+        disableDeck = true;
+
+        cardOneImg = cardOne.querySelector('img').src,
+        cardTwoImg = cardTwo.querySelector('img').src;
+        matchCards(cardOneImg, cardTwoImg);
+    };
+};
+
+
+function matchCards(img1, img2) {
+    let countCard = 0;
+    let resultNum, getNum, setNum;
+    let index = 0;
+    let balance = Number(walletBalance.innerHTML);
+    if (img1 == img2) {
+        matchedCard++;
+        index = matchedCard;
+
+        if (index) {
+            if (walletBalance.classList.contains('numX2')) {
+                setNum = honeyComb * matchedCard;
+                getNumX2(setNum);
+            } else {
+                getNum = balance + (honeyComb * matchedCard);
+                walletBalance.innerHTML = getNum;
+            }
+        }
+
+        for (let i = 1; i <= clickItem.length; i++) {
+            countCard = i;
+            resultNum = countCard / 2;
+        }
+
+        if (matchedCard == resultNum) {
+
+            setTimeout(() => {
+                shuffleCard();
+            }, 1000);
+        };
+        cardOne.removeEventListener('click', clickCard);
+        cardTwo.removeEventListener('click', clickCard);
+
+        cardOne = cardTwo = '';
+        return (disableDeck = false);
+    }
+
+    
+    setTimeout(() =>  {
+        cardOne.classList.add('shake');
+        cardTwo.classList.add('shake');
+    }, 200)
+
+
+    setTimeout(() => {
+        cardOne.classList.remove('visible', 'shake');
+        cardTwo.classList.remove('visible', 'shake');
+        cardOne = cardTwo = '';
+        disableDeck = false;
+    }, 600);
+};
+
+function shuffleCard() {
+    matchedCard = 0;
+    cardOne = cardTwo = '';
+    disableDeck = false;
+
+    clickItem.forEach(elem => {
+        elem.classList.remove('visible');
+
+        elem.addEventListener('click', clickCard);
+    });
+};
+
+
+// levelUp
+
+let nextBtn = document.querySelector('.arrow-next');
+let countLevel = document.querySelector('.count');
+
+nextBtn.addEventListener('click', levelUp);
+
+let level = 1;
+
+function levelUp () {
+    if (level < 5) {
+        countLevel.innerHTML =  ++level;
+        parentCard.innerHTML = '';
+        if (level == 2 ) {
+            doubleCard(imgBackSechZehn);
+        };
+
+        if (level == 3 ) {
+            parentCard.style.cssText = 'grid-template-columns: repeat(6, 1fr)';
+            doubleCard(imgBackVierUndZwanZig);
+        };
+
+        if (level == 4 ) {
+            parentCard.style.cssText = 'grid-template-columns: repeat(8, 1fr)';
+            doubleCard(imgBackZweiUndDreiSig);
+        };
+
+        if (level == 5 ) {
+            parentCard.style.cssText = 'grid-template-columns: repeat(10, 1fr)';
+            doubleCard(imgBackVierZig);
+        };
+    };
+};
+
+
 // placecholder card
 
-let plachecholder = document.querySelector('.game-placeholder');
 
-plachecholder.addEventListener('click', hintCard);
+let hint = document.querySelector('.placeholder');
 
-function hintCard() {
+hint.addEventListener('click', hintCard);
+
+function hintCard(e) {
+    let clickHint = e.target
     let img1, img2;
+    getBalance(clickHint)
     for (let item of clickItem) {
-        if (item.classList.contains('visible')) {
+        if (item.classList.contains('visible') ) {
             let cardsImg = document.querySelectorAll('.card');
             for (let elem of cardsImg) {
                 img1 = item.querySelector('img').src;
                 img2 = elem.querySelector('img').src;
                 if (img1 == img2) {
                     if (!elem.classList.contains('visible')) {
-                        elem.classList.add('active');
+                        elem.childNodes[0].classList.add('active');
+                        elem.childNodes[0].style.cssText = 'transition: all 0.5s';
+                        
                     }
                     setTimeout (() => {
-                        elem.classList.remove('active');
-                        elem.style.cssText = 'transition: all 0.5s'
+                        elem.childNodes[0].classList.remove('active');
+                        elem.childNodes[0].style.cssText = '';
                     }, 2000)
                 };
             };
@@ -151,36 +223,106 @@ function hintCard() {
 };
 
 
-// lvl up
+// start price 
 
-let nextBtn = document.querySelector('.arrow-next');
-let countLvL = document.querySelector('.count');
+let numPrice, getNumPrice;
 
-nextBtn.addEventListener('click', lvl);
-
-let count = 1;
-
-function lvl () {
-    if (count < 5) {
-        countLvL.innerHTML =  ++count;
-        parentCard.innerHTML = '';
-        if (count == 2 ) {
-            doubleCard(imgBackSechZehn);
-        };
-
-        if (count == 3 ) {
-            parentCard.style.cssText = 'grid-template-columns: repeat(6, 1fr)';
-            doubleCard(imgBackVierUndZwanZig);
-        };
-
-        if (count == 4 ) {
-            parentCard.style.cssText = 'grid-template-columns: repeat(8, 1fr)';
-            doubleCard(imgBackZweiUndDreiSig);
-        };
-
-        if (count == 5 ) {
-            parentCard.style.cssText = 'grid-template-columns: repeat(10, 1fr)';
-            doubleCard(imgBackVierZig);
+function getBalance(price) {
+    numPrice = Number(price.innerHTML);
+    let balance = Number(walletBalance.innerHTML);
+    let parent = price.parentElement;
+    for (let item of clickItem) {
+        if (item.classList.contains('visible') && balance >= numPrice ) {
+            getNumPrice = balance - numPrice;
+            walletBalance.innerHTML = getNumPrice;
+            return walletBalance.innerHTML = getNumPrice;
+        } else if (parent.classList.contains('multiplierX2') && balance >= numPrice ) {
+            getNumPrice = balance - numPrice;
+            walletBalance.innerHTML = getNumPrice;
+            return walletBalance.innerHTML = getNumPrice;
+        } else if (balance <= 0) {
+            walletBalance.style.color = 'red', 
+            walletBalance.innerHTML = '0';
+            setTimeout (() => {
+                walletBalance.style.color = 'black';
+            }, 1000)
+        } else if (balance < numPrice && balance > 0) {
+            price.classList.remove('visible');
+            walletBalance.style.color = 'red';
+            setTimeout (() => {
+                walletBalance.style.color = 'black';
+            }, 200)
         };
     };
+};
+
+
+// multiplierX2
+
+let multiplierX2 = document.querySelector('.multiplierX2');
+let sumHoneyComb;
+
+multiplierX2.addEventListener('click', numX2Card);
+
+function numX2Card(e) {
+    walletBalance.classList.add('numX2');
+    // if (walletBalance.classList.contains('numX2')) {
+    //     console.log('true')
+    //     let span = document.createElement('span');
+    //     span.innerHTML = 'X2';
+    //     span.style.marginLeft = '10px'
+    //     span.style.fontSize = '18px';
+    //     walletBalance.append(span);
+    // }
+    let clickNumX2 = e.target;
+    getBalance(clickNumX2);
+    timerX2();
+};
+
+function getNumX2(num) {
+    sumHoneyComb = num * 2;
+    walletBalance.innerHTML = Number(walletBalance.innerHTML) + sumHoneyComb;
+};
+
+
+// timer
+
+let gameMultiplierX2 = document.querySelector('.game-multiplierX2');
+
+let second = 30;
+let flagTimer = false;
+let div;
+
+function timerX2() {
+    if (!flagTimer) {
+        div  = document.createElement('div');
+        div.classList.add('timer');
+        gameMultiplierX2.append(div);
+        div.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+        div.style.width = '100%';
+        div.style.height = '100%';
+        div.style.position = 'absolute';
+        div.style.zIndex = '20';
+        div.style.top = '0';
+        div.style.borderRadius = '10px';
+        div.style.display = 'flex';
+        div.style.alignItems = 'center';
+        div.style.justifyContent = 'center';
+        div.style.fontSize = '20px';
+        div.style.color = 'white';
+        flagTimer = true;
+    };
+    
+    setTimeout(() => {
+        div.innerHTML = `0:${--second}`
+        if (second < 10) {
+            div.innerHTML = `0:0${second}`
+        };
+        if (second > 0) {
+            timerX2()
+        } else if (second == 0) {
+            div.style.display = 'none';
+            walletBalance.classList.remove('numX2');
+        };
+    }, 1000);   
 };
