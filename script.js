1000b7c1917ff783f1d3f13c17bbcdea80995d7b
Максим <77636349+Greiika@users.noreply.github.com>
2025-01-22 22:38:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function addingApoint(str) {
 let returnBalance = JSON.parse(localStorage.getItem('return-balance'));
 let balance = JSON.parse(localStorage.getItem('balance'));
 
-addingApoint(balance);
+addingApoint(String(balance));
 
 // animation balance
 
