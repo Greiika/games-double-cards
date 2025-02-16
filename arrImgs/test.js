53cb92ce540284d5e2cd26100e0a5407c9f21916
Max <kotiik97@mail.ru>
2025-02-16 23:09:46 +0300

    update

--- arrImgs/test.js
+++ arrImgs/test.js
@@ -1,6 +1,8 @@
+let randomId = '' + Math.random().toString(16).slice(2);
+
 let shopArr = [
     {
-        id: 1,
+        id: randomId,
         title: 'Набор карточке из 4-х пар',
         image: 'img/nature/img-19.jpg',
         btn: 'Купить',
---
+++ development.js
@@ -0,0 +1,2 @@
+let randomId = '' + Math.random().toString(16).slice(2);
+console.log(randomId)
